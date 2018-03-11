24a5c7cf58a711b01e74b8e169ec8aee73fbb4c3
Ahmed Dea <ad.ahmed.dea2022@gmail.com>
2018-03-11 05:46:12 +0200

    Create Member and Post

--- Forums/models.py
+++ Forums/models.py
@@ -4,31 +4,13 @@ class models:
         self.name = name
         self.age = age
 
-    def data_models (self):
-        print "Member Name: " ,self.name
-        print "Age of the Member: " ,self.age
-        print "*"*30
-
-
-name1 = "Ahmed"
-name2 = "Eosef"
-
-
-mpdels1 = models(name1, 30)
-mpdels2 = models(name2, 18)
-
-models.data_models(mpdels1)
-models.data_models(mpdels2)
-
 
 class post:
 
-    def __init__(self, delete, read, create, update):
+    def __init__(self, Address, Body):
 
-        self.delete = delete
-        self.read = read
-        self.create = create
-        self.update =update
+        self.Address = Address
+        self.Body = Body
 
 
 
